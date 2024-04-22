9db0bbcaec777efd020d3c3751cb90d3dfdb825b
AnyaAkhmatova <akhmatova-anna-2002@mail.ru>
2024-04-22 21:31:02 +0300

    Commit

--- ss/metric/accuracy_metric.py
+++ ss/metric/accuracy_metric.py
@@ -1,10 +1,15 @@
+import torch
+
 from .base_metric import BaseMetric
 
 
 class ACC(BaseMetric):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, device, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.device = device
 
-    def __call__(self, logits, target_id, **kwargs):
+    def __call__(self, logits=None, target_id=None, **kwargs):
+        if logits is None:
+            return torch.tensor([0.0], device=self.device)
         preds = logits.argmax(-1)
         return (preds == target_id).float().mean()
